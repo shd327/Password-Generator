438623e76ea0b143242d49dc1a8eb4c8ffd9d719
Hiro Protagonist <shd327@gmail.com>
2022-10-27 15:46:50 -0600

    added confirm prompts for each criteria

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var isLowerCase = confirm("Do you want Lower case letters in your password");
+var isUpperCase = confirm("Do you want Upper case letters in your password");
+var isNumber = confirm("Do you want numbers in your password");
+var isSpecialCharacters = confirm("Do you want special characters in your password");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +16,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// length of password 8 -128
+
+// lowercase
+//uppercase
+// number
+// special characters
+
