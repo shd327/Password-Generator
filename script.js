c36823cf12eb956ca8de0f6580b5e6e70601d3aa
Hiro Protagonist <shd327@gmail.com>
2022-10-28 08:38:41 -0600

    added math function

--- script.js
+++ script.js
@@ -14,45 +14,72 @@ var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M"
 var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"];
   newpassword = [];
-  passwordTemp =0;
+passwordTemp = 0;
+var finalpass = "";
 // Write password to the #password input
 
 function numberGenerator() {
-  for (let i = 0; i < numberArray.length; i++){
-    newpassword.push[i]
+
+  for (let i = 0; i < numberArray.length; i++) {
+    newpassword.push(numberArray[i])
   }
+
 }
 function lowerCaseGenerator() {
-  for (let i = 0; i < upperCaseArray.length; i++){
-    newpassword.push[i]
+  for (let i = 0; i < lowerCaseArray.length; i++) {
+    newpassword.push(lowerCaseArray)
   }
 }
 function specialCharacterGenerator() {
-  for (let i = 0; i < specialCharArray.length; i++){
-    newpassword.push[i]
+  for (let i = 0; i < specialCharArray.length; i++) {
+    newpassword.push(specialCharArray)
   }
 }
 function upperCaseGenerator() {
-  for (let i = 0; i < upperCaseArray.length; i++){
-    newpassword.push[i]
+  for (i = 0; i < upperCaseArray.length; i++) {
+    newpassword.push(upperCaseArray)
   }
 }
-function writePassword() {
-
-  var lengthofPassword = prompt("please enter a password from 8-128");
+function generatePassword() {
+    var lengthofPassword = prompt("please enter a password from 8-128");
   if (lengthofPassword >= 8 && lengthofPassword <= 128) {
     passwordTemp = lengthofPassword;
-     alert("good")
+    
   }
   else {
      alert("your input is invalid please try again")
     writePassword() 
   }
   
-var isLowerCase = confirm("Do you want Lower case letters in your password");
-var isUpperCase = confirm("Do you want Upper case letters in your password");
-var isNumber = confirm("Do you want numbers in your password");
-var isSpecialCharacters = confirm("Do you want special characters in your password");
+  var isLowerCase = confirm("Do you want Lower case letters in your password");
+
+  if (isLowerCase) {
+    lowerCaseGenerator()
+    console.log(newpassword)
+  }
+  var isUpperCase = confirm("Do you want Upper case letters in your password");
+    if (isUpperCase) {
+    upperCaseGenerator()
+  }
+  var isNumber = confirm("Do you want numbers in your password");
+      if (isNumber) {
+    numberGenerator()
+  }
+  var isSpecialCharacters = confirm("Do you want special characters in your password");
+      if (isSpecialCharacters) {
+    specialCharacterGenerator()
+  }
+  console.log(newpassword)
+
+  for (let i = 0; i < newpassword.length; i++){
+     var randomLength = Math.floor(Math.random() * newpassword.length);
+    finalpass += randomLength;
+    console.log(finalpass)
+  }
+}
+function writePassword() {
+
+
 
   var password = generatePassword();
 
